1d87306b2d635bbd2b9121ea155048bc92587d3c
Anton <anton98156789@gmail.com>
2022-10-04 21:15:33 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿/*int number = new Random().Next (100,1000);
+
+Console.WriteLine(number);
+int num1 = number/100;
+int num2 = number%10;
+
+Console.WriteLine($"{num1}{num2}");*/
+
+/*Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. 
+Если второе число не кратно числу первому, то программа выводит остаток от деления.*/
+
+/*Console.WriteLine("Введите первое число");
+int num1 = int.Parse (Console.ReadLine()!);
+Console.WriteLine("Введите второе число");
+int num2 = int.Parse (Console.ReadLine()!);
+
+if(num1%num2==0){
+    Console.WriteLine("Второе число является кратным первому");
+}
+
+else{
+    Console.WriteLine(num1%num2);
+}*/
+
+/*
+Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+14  ->  нет
+46  ->  нет
+161 ->  да*/
+
+/*
+Console.WriteLine("Введите число");
+int num1 = int.Parse (Console.ReadLine()!);
+
+if((num1%7==0)&&(num1%23==0)){
+    Console.WriteLine("Числое кратное одновременно 7 и 23");
+}
+else{
+    Console.WriteLine("Условие невыполнено");
+}*/
+
+/* Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.*/
+
+/*
+Console.WriteLine("Введите первое число");
+int num1 = int.Parse (Console.ReadLine()!);
+Console.WriteLine("Введите второе число");
+int num2 = int.Parse (Console.ReadLine()!);
+
+if(num1*num1==num2){
+    Console.WriteLine("Второе число является квадратом первого");
+}
+else if(num2*num2==num1){
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else{
+     Console.WriteLine("Одно число не является квадратом другого");
+} */
